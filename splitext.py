7b9d5fa2f5d5484eb9d1ecc1c7957262044af2ee
Pokeshah <89949620+Pokeshah@users.noreply.github.com>
2023-06-20 00:11:10 -0700

    add "[split]" functionality to split
    1. force a split whenever you add a "[split]" tag to help with block quotes
    2. make the % completed a subheading to draw attention to it
    3. edit the message before posting a new one for a more fluent discord animation

--- splitext.py
+++ splitext.py
@@ -30,6 +30,15 @@ def edit(messageid, message, auth, channel):
 	print(r.status_code)
 	return r
 
+def delete(messageid, auth, channel):
+	r = requests.delete(
+			url=f"https://discord.com/api/v9/channels/{channel}/messages/{messageid}",
+			headers=({'authorization': auth})
+		)
+	print(r.status_code,messageid)
+	return r
+
+
 
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'splittext.txt'), 'r') as file:
 	text = file.read()
@@ -37,30 +46,38 @@ with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'splittext.tx
 # print(text)
 
 
-
-
 def split_text(text):
-	# Split the text into sentences
-	sentences = re.split(r'[\n]\s*', text)
+    # Split the text into chunks using the string "[split]"
+    chunks = text.split("[split]")
 
-	# Initialize the list of chunks
-	chunks = []
-	current_chunk = ''
+    # Initialize the list of chunks
+    result = []
 
-	# Loop through the sentences and add them to the current chunk
-	for sentence in sentences:
-		# If adding the sentence to the current chunk would make it too long, add the current chunk to the list of chunks and start a new one
-		if len(current_chunk) + len(sentence) > 1990:
-			chunks.append(current_chunk)
-			current_chunk = ''
+    # Loop through the chunks and add them to the result list
+    for chunk in chunks:
+        # Split the chunk into sentences
+        sentences = re.split(r'[\n]\s*', chunk)
 
-		# Add the sentence to the current chunk
-		current_chunk += sentence + '\n'
+        # Initialize the list of chunks for this chunk's sentences
+        current_chunk = ''
 
-	# Add the final chunk to the list of chunks
-	chunks.append(current_chunk)
-	print(chunks)
-	return chunks
+        # Loop through the sentences and add them to the current chunk
+        for sentence in sentences:
+            # If adding the sentence to the current chunk would make it too long, add the current chunk to the result list and start a new one
+            if len(current_chunk) + len(sentence) > 1940:
+                result.append(current_chunk)
+                current_chunk = ''
+
+            # Add the sentence to the current chunk
+            current_chunk += sentence + '\n'
+
+        # Add the final chunk to the result list
+        result.append(current_chunk)
+
+    # Print the result list
+    print(result)
+
+    return result
 
 
 # Get the list of chunks
@@ -73,25 +90,25 @@ total_chunks = len(chunks)
 percent_sent = 1 / total_chunks * 100
 
 # Send the chunk along with the percentage
-message = f'{chunks[0]}\n({percent_sent:.2f}% sent)'
+message = f'{chunks[4]}\n## ({percent_sent:.2f}% sent)'
 messageid = json.loads(send(message, token, sendto).text)["id"]
 
 try:
-	for i in range(1, total_chunks):
+	for i in range(5, total_chunks):
 		# Calculate the percentage of the file that has been sent
 		percent_sent = (i + 1) / total_chunks * 100
 	
 		# Send the chunk along with the percentage
 		if int(percent_sent) != 100:
-			message = f'{chunks[i]}\n({percent_sent:.2f}% sent)'
+			message = f'{chunks[i]}\n## ({percent_sent:.2f}% sent)'
 		else:
 			message = chunks[i]
 		tempmessageid = messageid
-		messageid = json.loads(send(message, token, sendto).text)["id"]
 		edit_message = chunks[i-1]
 		edit_response = edit(tempmessageid, edit_message, token, sendto)
-	
+		messageid = json.loads(send(message, token, sendto).text)["id"]
+		print(i)
 		# Wait for 2 seconds
 		time.sleep(random.uniform(2.5, 3.5))
 except KeyboardInterrupt:
-    pass 
+    pass
